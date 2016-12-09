eb23b50ac3eb8f1aa2886898803bbf3fb2d863ef
Rodrigo Tschope <rodrigo.tschope@ag2nurun.com.br>
2016-12-09 15:02:33 -0200

    Cara, está ganhando. Não vou mexer mais, juro...

--- bit.js
+++ bit.js
@@ -1,90 +1,62 @@
-var tempo = 1800; //1.2 segundos
+var tempo = 2;
 var intervalo = 0;
 var qtsPerdidas = 0;
-var valorInicial = parseFloat($("#balance").text());
-var valorFinal = 0;
-var maxBit = 0.00000064;
-var maisToshi = 0.00000001;
-var valMultip = 2.25; //Valor multiplicador básico
-var arregao = false;
-function perdeu() {
-    return $("#double_your_btc_bet_lose").text().indexOf("lose") > -1;
-}
-function aposta() {
-    //var numb = Math.floor((Math.random() * 10) + 1);
-    //if (numb >= 6) {
-        $('#double_your_btc_bet_hi_button').click();
-    //} else {
-    //    $('#double_your_btc_bet_lo_button').click();
-    //}
-}
+var maxAposta = 0.00000256;
+var carteira = parseFloat($("#balance").text());
+var perdaTotal = 5; //%
+
+function perdeu() { return $("#double_your_btc_bet_lose").text().indexOf("lose")>-1; }
+function aposta() { $('#double_your_btc_bet_lo_button').click(); }
 function maxima() {
-    return parseFloat($("#double_your_btc_stake").val()) > maxBit;
+    return parseFloat($("#double_your_btc_stake").val()) > maxAposta;
 }
 function zera() {
     qtsPerdidas = 0;
     $("#double_your_btc_min").click();
+    //$("#double_your_btc_stake").val("0.00000032");
 }
-function aumenta() {
-    $("#double_your_btc_2x").click();
-    //parseFloat($("#double_your_btc_stake").val(parseFloat($("#double_your_btc_stake").val()) + maisToshi));
-}
+function aumenta() { $("#double_your_btc_2x").click(); }
 function aumentaVai() {
     aumenta();
-    //aumenta();
+    aumenta();
     aposta();
 }
-function poemMultip() {
-    //var Multi = parseFloat(Math.floor(Math.random() * 3) + 3 + "." + Math.floor(Math.random() * 99));
-    var Multi = valMultip;
-    $("#double_your_btc_payout_multiplier").val(Multi);
-    $("#double_your_btc_payout_multiplier").text(Multi);
-}
-function ligaSom() {
-    $('#manual_enable_sounds').prop('checked', true);
-}
-function mute() {
-    $('#manual_enable_sounds').prop('checked', false);
-}
 function verifica() {
-    if (perdeu()) {
+    if(perdeu()) {
         qtsPerdidas++;
-        if (qtsPerdidas >= 2) {
-            if (maxima()) {
-                if(!arregao){
+        if(qtsPerdidas>=2) {
+            if(maxima()) {
+                if(parseFloat($("#balance").text())>=carteira-(carteira*perdaTotal/100)) {
+                    pusha("Perdeu " + perdaTotal + "%.");
+                    stop();
+                } else {
+                    pusha("Apostou o máximo " + maxAposta + " BTC.");
                     zera();
                     aposta();
-                }else{
-                    stop();
-                    console.info("Perdeu muito");
                 }
             } else {
                 aumentaVai();
             }
         }
     } else {
+        pusha("Carteira " + parseFloat($("#balance").text()) + " BTC.");
         zera();
         aposta();
     }
 }
 function stop() {
-    valorFinal = parseFloat($("#balance").text());
-    console.info('Valor Inicial:' + valorInicial);
-    console.info('Valor Final:' + valorFinal);
-    if ((valorFinal - valorInicial) < 0) {
-        console.info("PERDEU!");
-    } else {
-        console.info("GANHOU!");
-    }
     qtsPerdidas = 0;
     clearInterval(intervalo);
     intervalo = 0;
+    console.info("Perdeu muito: " + perdaTotal + " %");
 }
 function run() {
-    console.info('Valor Inicial:' + valorInicial);
-    poemMultip();
-    intervalo = setInterval(function () {
+    carteira = parseFloat($("#balance").text());
+    intervalo = setInterval(function(){
         verifica();
-    }, tempo);
+    }, tempo*1000);
+}
+function pusha(texto) {
+    console.info(texto);
 }
 run();
